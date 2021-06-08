d5342932e3c9148458b1c0ce703a7a84149fabb0
DivakarRaju <divakarrj875@gmail.com>
2021-06-08 23:03:30 +0530

    fixed some issues

--- aws_infrastructure.groovy
+++ aws_infrastructure.groovy
@@ -1,3 +1,12 @@
+BUILD_INFRASTRUCTURE = 'scm_terraform'
+PROJECT_NAME = 'scm'
+SCM_GIT_URL = 'https://github.com/nawinkumar94/Terraform-Aws-Two-Tier-Application.git'
+BRANCH_NAME = 'main'
+TERRAFORM_VERSION = '0.13.3'
+REGION = 'ap-south-1'
+CREDENTIALS_ID =
+OPERATION = 'OPERATION
+
 pipelineJob(BUILD_INFRASTRUCTURE){
     displayName(PROJECT_NAME)
     description('Deploys the Terraform configurations')
@@ -5,5 +14,17 @@ pipelineJob(BUILD_INFRASTRUCTURE){
         daysToKeep(30)
         numToKeep(10)
     }
-    
+    parameters{
+        stringParam('BRANCH_NAME', BRANCH_NAME, 'SCM Branch Details')
+        stringParam('TERRAFORM_VERSION', TERRAFORM_VERSION, 'Version of Terraform to Deploy')
+        stringParam('SCM_GIT_URL', SCM_GIT_URL, 'Terraform Project Infrastructure')
+        choiceParam('OPERATION', ['apply', 'destroy'])
+        stringParam('REGION', defaultValue = REGION)
+        stringParam('BUCKET_NAME', BUCKET_NAME)
+        credentialsParam('CREDENTIALS_ID'){
+            defaultValue('CREDENTIALS_ID')
+        }
+    }
+
+
 }
